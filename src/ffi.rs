db3782990491772153af2f8fc30c53d0f5efd9c9
Andrew Poelstra <apoelstra@wpsoftware.net>
2014-08-11 19:26:14 -0700

    Add CC0 license and header to all files

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,3 +1,17 @@
+// Bitcoin secp256k1 bindings
+// Written in 2014 by
+//   Dawid Ciężarkiewicz
+//   Andrew Poelstra
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication
+// along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//
 
 //! FFI bindings
 use libc::{c_int, c_uchar};
