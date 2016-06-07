f38323d90bbfaa0901c5a250993f775eb90e5b7e
Robert Habermeier <rphmeier@gmail.com>
2016-06-07 16:54:15 +0200

    restore ext.c

--- build.rs
+++ build.rs
@@ -44,7 +44,7 @@ fn main() {
 
     // secp256k1
     base_config.file("depend/secp256k1/contrib/lax_der_parsing.c")
-               .file("depend/secp256k1/src/secp256k1.c")
+	       .file("depend/secp256k1/src/ext.c")
                .compile("libsecp256k1.a");
 }
 
