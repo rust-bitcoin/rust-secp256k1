0cca5174cffc6e8896c9098ba448db9faf9faa36
debris <marek.kotewicz@gmail.com>
2017-03-03 15:56:28 +0100

    fixed cross compilation from linux for android

--- build.rs
+++ build.rs
@@ -27,10 +27,26 @@ use std::env;
 use std::ffi::OsString;
 use std::path::PathBuf;
 
-const ANDROID_AARCH64_COMPILER: &'static str = "toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/bin";
-const ANDROID_ARM_COMPILER: &'static str = "toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin";
-const ANDROID_I686_COMPILER: &'static str = "toolchains/x86-4.9/prebuilt/darwin-x86_64/bin";
+#[cfg(target_os = "macos")]
+const OS: &'static str = "darwin";
+#[cfg(target_os = "linux")]
+const OS: &'static str = "linux";
+#[cfg(target_os = "windows")]
+const OS: &'static str = "windows";
+
 const ANDROID_INCLUDE: &'static str = "platforms/android-21/arch-arm64/usr/include";
+
+fn android_aarch_compiler() -> String {
+	"toolchains/aarch64-linux-android-4.9/prebuilt/".to_owned() + OS + "-x86_64/bin"
+}
+
+fn android_arm_compiler() -> String {
+	"toolchains/arm-linux-androideabi-4.9/prebuilt/".to_owned() + OS + "-x86_64/bin"
+}
+
+fn android_i686_compiler() -> String {
+	"toolchains/x86-4.9/prebuilt/".to_owned() + OS + "-x86_64/bin"
+}
 
 fn concat_paths(first: &str, second: &str) -> PathBuf {
 	let mut path = PathBuf::from(first);
@@ -42,9 +58,9 @@ fn setup_android(config: &mut gcc::Config) {
 	let path = env::var_os("PATH").unwrap_or_else(OsString::new);
 	let ndk_home = env::var("NDK_HOME").expect("NDK_HOME is not set");
 	let mut paths = env::split_paths(&path).collect::<Vec<_>>();
-	paths.push(concat_paths(&ndk_home, ANDROID_AARCH64_COMPILER));
-	paths.push(concat_paths(&ndk_home, ANDROID_ARM_COMPILER));
-	paths.push(concat_paths(&ndk_home, ANDROID_I686_COMPILER));
+	paths.push(concat_paths(&ndk_home, &android_aarch_compiler()));
+	paths.push(concat_paths(&ndk_home, &android_arm_compiler()));
+	paths.push(concat_paths(&ndk_home, &android_i686_compiler()));
 
 	let new_path = env::join_paths(paths).expect("all paths were created using PathBuf's; qed");
 	env::set_var("PATH", new_path);
