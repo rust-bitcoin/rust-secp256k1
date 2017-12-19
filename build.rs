fa7dc40254901600f1664715cc797bf55e34b513
Andrew Poelstra <apoelstra@wpsoftware.net>
2017-12-19 22:40:24 +0000

    rename gcc::Config to gcc::Build

--- build.rs
+++ build.rs
@@ -24,7 +24,7 @@
 extern crate gcc;
 
 fn main() {
-    let mut base_config = gcc::Config::new();
+    let mut base_config = gcc::Build::new();
     base_config.include("depend/secp256k1/")
                .include("depend/secp256k1/include")
                .include("depend/secp256k1/src")
