5a27abab52500f23431b71d9592631b39a4bc84b
Andrew Poelstra <apoelstra@wpsoftware.net>
2018-07-30 03:11:43 +0000

    switch to 64-bit compilation; add flag for 32-bit

--- build.rs
+++ build.rs
@@ -24,6 +24,19 @@
 extern crate cc;
 
 fn main() {
+    // Check whether we can use 64-bit compilation
+    #[cfg(target_pointer_width = "64")]
+    let use_64bit_compilation = {
+        cc::Build::new().file("depend/check_uint128_t.c")
+                        .cargo_metadata(false)
+                        .try_compile("check_uint128_t")
+                        .is_ok()
+    };
+    #[cfg(not(target_pointer_width = "64"))]
+    let use_64bit_compilation = false;
+
+
+    // Actual build
     let mut base_config = cc::Build::new();
     base_config.include("depend/secp256k1/")
                .include("depend/secp256k1/include")
@@ -35,13 +48,18 @@ fn main() {
                .define("USE_NUM_NONE", Some("1"))
                .define("USE_FIELD_INV_BUILTIN", Some("1"))
                .define("USE_SCALAR_INV_BUILTIN", Some("1"))
-               // TODO these should use 64-bit variants on 64-bit systems
-               .define("USE_FIELD_10X26", Some("1"))
-               .define("USE_SCALAR_8X32", Some("1"))
                .define("USE_ENDOMORPHISM", Some("1"))
-               // These all are OK.
                .define("ENABLE_MODULE_ECDH", Some("1"))
                .define("ENABLE_MODULE_RECOVERY", Some("1"));
+
+    if use_64bit_compilation {
+        base_config.define("USE_FIELD_5X52", Some("1"))
+                   .define("USE_SCALAR_4X64", Some("1"))
+                   .define("HAVE___INT128", Some("1"));
+    } else {
+        base_config.define("USE_FIELD_10X26", Some("1"))
+                   .define("USE_SCALAR_8X32", Some("1"));
+    }
 
     // secp256k1
     base_config.file("depend/secp256k1/contrib/lax_der_parsing.c")
