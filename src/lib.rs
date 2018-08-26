572adb2873132383eb971558e874fe2caede1905
Andrew Poelstra <apoelstra@wpsoftware.net>
2018-08-26 18:39:41 +0000

    add `FromStr` implementation for key types

--- src/lib.rs
+++ src/lib.rs
@@ -708,36 +708,50 @@ impl<C: Verification> Secp256k1<C> {
     }
 }
 
+/// Utility function used to parse hex into a target u8 buffer. Returns
+/// the number of bytes converted or an error if it encounters an invalid
+/// character or unexpected end of string.
+fn from_hex(hex: &str, target: &mut [u8]) -> Result<usize, ()> {
+    if hex.len() % 2 == 1 || hex.len() > target.len() * 2 {
+        return Err(());
+    }
+
+    let mut b = 0;
+    let mut idx = 0;
+    for c in hex.bytes() {
+        b <<= 4;
+        match c {
+            b'A'...b'F' => b |= c - b'A' + 10,
+            b'a'...b'f' => b |= c - b'a' + 10,
+            b'0'...b'9' => b |= c - b'0',
+            _ => return Err(()),
+        }
+        if (idx & 1) == 1 {
+            target[idx / 2] = b;
+            b = 0;
+        }
+        idx += 1;
+    }
+    Ok(idx / 2)
+}
+
+
 #[cfg(test)]
 mod tests {
     use rand::{Rng, thread_rng};
 
     use key::{SecretKey, PublicKey};
+    use super::from_hex;
     use super::constants;
     use super::{Secp256k1, Signature, RecoverableSignature, Message, RecoveryId};
     use super::Error::{InvalidMessage, IncorrectSignature, InvalidSignature};
 
     macro_rules! hex {
-        ($hex:expr) => {
-            {
-                let mut vec = Vec::new();
-                let mut b = 0;
-                for (idx, c) in $hex.as_bytes().iter().enumerate() {
-                    b <<= 4;
-                    match *c {
-                        b'A'...b'F' => b |= c - b'A' + 10,
-                        b'a'...b'f' => b |= c - b'a' + 10,
-                        b'0'...b'9' => b |= c - b'0',
-                        _ => panic!("Bad hex"),
-                    }
-                    if (idx & 1) == 1 {
-                        vec.push(b);
-                        b = 0;
-                    }
-                }
-                vec
-            }
-        }
+        ($hex:expr) => ({
+            let mut result = vec![0; $hex.len() / 2];
+            from_hex($hex, &mut result).expect("valid hex string");
+            result
+        });
     }
 
     #[test]
