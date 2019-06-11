179f35e0ba9c21d944e19dcc530bbb0f632c26e5
Jonas Nick <jonasd.nick@gmail.com>
2019-06-11 20:32:54 +0000

    Move libsecp USE_ENDOMORPHISM option behind non-default feature flag

--- build.rs
+++ build.rs
@@ -52,9 +52,10 @@ fn main() {
                .define("USE_NUM_NONE", Some("1"))
                .define("USE_FIELD_INV_BUILTIN", Some("1"))
                .define("USE_SCALAR_INV_BUILTIN", Some("1"))
-               .define("USE_ENDOMORPHISM", Some("1"))
                .define("ENABLE_MODULE_ECDH", Some("1"));
 
+    #[cfg(feature = "endomorphism")]
+    base_config.define("USE_ENDOMORPHISM", Some("1"));
     #[cfg(feature = "recovery")]
     base_config.define("ENABLE_MODULE_RECOVERY", Some("1"));
 
