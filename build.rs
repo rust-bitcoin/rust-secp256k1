f1d85b8b8a46027b854afa830e48bac9ceab839f
Demi M. Obenour <demi@parity.io>
2019-10-14 09:27:10 -0400

    Upgrade dependencies and use Git submodule for libsecp256k1

--- build.rs
+++ build.rs
@@ -137,7 +137,7 @@ fn main() {
 
 	// secp256k1
 	base_config.file("depend/secp256k1/contrib/lax_der_parsing.c")
-		.file("depend/secp256k1/src/ext.c")
+		.file("depend/ext.c")
 		.compile("libsecp256k1.a");
 }
 
