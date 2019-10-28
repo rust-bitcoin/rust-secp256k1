e28f7567889c6c983d6aa45ef833d6fa165c50df
Elichai Turkel <elichai.turkel@gmail.com>
2019-10-28 22:09:59 +0200

    Added an example for signing and verification

---
+++ examples/sign_verify.rs
@@ -0,0 +1,35 @@
+extern crate bitcoin_hashes;
+extern crate secp256k1;
+
+use bitcoin_hashes::{sha256, Hash};
+use secp256k1::{Error, Message, PublicKey, Secp256k1, SecretKey, Signature, Signing, Verification};
+
+fn verify<C: Verification>(secp: &Secp256k1<C>, msg: &[u8], sig: [u8; 64], pubkey: [u8; 33]) -> Result<bool, Error> {
+    let msg = sha256::Hash::hash(msg);
+    let msg = Message::from_slice(&msg)?;
+    let sig = Signature::from_compact(&sig)?;
+    let pubkey = PublicKey::from_slice(&pubkey)?;
+
+    Ok(secp.verify(&msg, &sig, &pubkey).is_ok())
+}
+
+fn sign<C: Signing>(secp: &Secp256k1<C>, msg: &[u8], seckey: [u8; 32]) -> Result<Signature, Error> {
+    let msg = sha256::Hash::hash(msg);
+    let msg = Message::from_slice(&msg)?;
+    let seckey = SecretKey::from_slice(&seckey)?;
+    Ok(secp.sign(&msg, &seckey))
+}
+
+fn main() {
+    let secp = Secp256k1::new();
+
+    let seckey = [59, 148, 11, 85, 134, 130, 61, 253, 2, 174, 59, 70, 27, 180, 51, 107, 94, 203, 174, 253, 102, 39, 170, 146, 46, 252, 4, 143, 236, 12, 136, 28];
+    let pubkey = [2, 29, 21, 35, 7, 198, 183, 43, 14, 208, 65, 139, 14, 112, 205, 128, 231, 245, 41, 91, 141, 134, 245, 114, 45, 63, 82, 19, 251, 210, 57, 79, 54];
+    let msg = b"This is some message";
+
+    let signature = sign(&secp, msg, seckey).unwrap();
+
+    let serialize_sig = signature.serialize_compact();
+
+    assert!(verify(&secp, msg, serialize_sig, pubkey).unwrap());
+}
