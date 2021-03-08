3511f4fb8046597c561f4c6ca926cab7bf2bc72f
Kirill Fomichev <fanatid@ya.ru>
2021-03-08 20:12:44 +0300

    Define field/scalar impl in secp256k1-sys/build.rs

--- secp256k1-sys/build.rs
+++ secp256k1-sys/build.rs
@@ -51,6 +51,23 @@ fn main() {
     #[cfg(feature = "recovery")]
     base_config.define("ENABLE_MODULE_RECOVERY", Some("1"));
 
+    // field/scalar implementations (32bit/64bit)
+    match env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap().as_str() {
+        "32" => {
+            base_config
+                .define("USE_FIELD_10X26", "1")
+                .define("USE_SCALAR_8X32", "1");
+        }
+        "64" => {
+            base_config
+                .define("HAVE___INT128", "1")
+                .define("USE_ASM_X86_64", "1")
+                .define("USE_FIELD_5X52", "1")
+                .define("USE_SCALAR_4X64", "1");
+        }
+        width @ _ => panic!("Unknown pointer width: {}", width),
+    }
+
     match &env::var("TARGET").unwrap() as &str {
         "wasm32-unknown-unknown"|"wasm32-wasi" =>
             { base_config.include("wasm-sysroot"); },
