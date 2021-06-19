999d165c68f448a6444fb51895c6e75c7226d297
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-06-19 11:18:10 +0200

    FFI for pubkey comparison ops

--- secp256k1-sys/src/lib.rs
+++ secp256k1-sys/src/lib.rs
@@ -379,6 +379,12 @@ extern "C" {
                                         tweak: *const c_uchar)
                                         -> c_int;
 
+    #[cfg_attr(not(rust_secp_no_symbol_renaming), link_name = "rustsecp256k1_v0_5_0_ec_pubkey_cmp")]
+    pub fn secp256k1_ec_pubkey_cmp(cx: *const Context,
+                                   pubkey1: *const PublicKey,
+                                   pubkey2: *const PublicKey)
+                                   -> c_int;
+
     #[cfg_attr(not(rust_secp_no_symbol_renaming), link_name = "rustsecp256k1_v0_5_0_keypair_sec")]
     pub fn secp256k1_keypair_sec(cx: *const Context,
                                  output_seckey: *mut c_uchar,
@@ -390,6 +396,12 @@ extern "C" {
                                  output_pubkey: *mut PublicKey,
                                  keypair: *const KeyPair)
                                  -> c_int;
+
+    #[cfg_attr(not(rust_secp_no_symbol_renaming), link_name = "rustsecp256k1_v0_5_0_xonly_pubkey_cmp")]
+    pub fn secp256k1_xonly_pubkey_cmp(cx: *const Context,
+                                      pubkey1: *const XOnlyPublicKey,
+                                      pubkey2: *const XOnlyPublicKey)
+                                      -> c_int;
 }
 
 #[cfg(not(fuzzing))]
