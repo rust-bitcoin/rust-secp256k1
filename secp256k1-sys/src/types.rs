f6a19290fc25730783a6b7be969294eb1fc31658
Tobin Harding <me@tobin.cc>
2022-01-11 17:41:11 +1100

    Use hyperlinks
    Clippy emits two warnings of type:
    
     warning: this URL is not a hyperlink
    
    As suggested, add pointy brackets to the links.

--- secp256k1-sys/src/types.rs
+++ secp256k1-sys/src/types.rs
@@ -10,7 +10,7 @@ pub type size_t = usize;
 /// The way we use it makes it fine either way but this type shouldn't be used outside of the library.
 pub type c_char = i8;
 
-/// This is an exact copy of https://doc.rust-lang.org/core/ffi/enum.c_void.html
+/// This is an exact copy of <https://doc.rust-lang.org/core/ffi/enum.c_void.html>
 /// It should be Equivalent to C's void type when used as a pointer.
 ///
 /// We can replace this with `core::ffi::c_void` once we update the rustc version to >=1.30.0.
