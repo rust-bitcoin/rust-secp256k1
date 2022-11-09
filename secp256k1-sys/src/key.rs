d445404e2efe36a3811fab9e7dcc1742d0bcc5df
Tobin C. Harding <me@tobin.cc>
2022-11-09 11:37:29 +1100

    secp256k1-sys: Improve docs on the key module
    Make explicit that we do not guarantee the byte layout of the inner
    array of the key types.

--- secp256k1-sys/src/key.rs
+++ secp256k1-sys/src/key.rs
@@ -1,17 +1,28 @@
 use crate::types::*;
 use crate::{impl_array_newtype, impl_raw_debug};
 
-/// Library-internal representation of a Secp256k1 public key
+/// Library-internal representation of a Secp256k1 public key.
+///
+/// The inner array is to be considered opaque, it is passed across the FFI boundary and as such we
+/// make no guarantees about the byte layout or stability across library versions or architectures.
 #[repr(C)]
 pub struct PublicKey([c_uchar; 64]);
 impl_array_newtype!(PublicKey, c_uchar, 64);
 impl_raw_debug!(PublicKey);
 
+/// Library-internal representation of a Secp256k1 x-only public key.
+///
+/// The inner array is to be considered opaque, it is passed across the FFI boundary and as such we
+/// make no guarantees about the byte layout or stability across library versions or architectures.
 #[repr(C)]
 pub struct XOnlyPublicKey([c_uchar; 64]);
 impl_array_newtype!(XOnlyPublicKey, c_uchar, 64);
 impl_raw_debug!(XOnlyPublicKey);
 
+/// Library-internal representation of a Secp256k1 key pair.
+///
+/// The inner array is to be considered opaque, it is passed across the FFI boundary and as such we
+/// make no guarantees about the byte layout or stability across library versions or architectures.
 #[repr(C)]
 pub struct KeyPair([c_uchar; 96]);
 impl_array_newtype!(KeyPair, c_uchar, 96);
