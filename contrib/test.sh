e358d993d7ab8fd965705bc910ab42ccd5ad39b5
Tobin C. Harding <me@tobin.cc>
2023-07-21 11:39:08 +1000

    Run WASM tests from test wrapper script
    The `wasm-pack` command does not honour `cargo` flags passed to it so we
    cannot use `--locked` and test against pre-made lock files. Instead just
    run the WASM test from the test script wrapper.

--- contrib/test.sh
+++ contrib/test.sh
@@ -4,4 +4,18 @@ set -ex
 
 REPO_DIR=$(git rev-parse --show-toplevel)
 
+# Webassembly stuff
+#
+# The wasm-pack command does not correctly pass args to cargo so we cannot use --locked and test
+# with per-commited lockfiles (recent/minimal). Just run the WASM tests from here instead.
+if [ "$DO_WASM" = true ]; then
+    clang --version
+    CARGO_TARGET_DIR=wasm cargo install --force wasm-pack
+    printf '\n[lib]\ncrate-type = ["cdylib", "rlib"]\n' >> Cargo.toml
+    CC=clang wasm-pack build
+    CC=clang wasm-pack test --node
+
+    exit 0
+fi
+
 $REPO_DIR/contrib/_test.sh
