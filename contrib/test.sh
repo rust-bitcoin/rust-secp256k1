b1b0bc85e2272ed882b22c34ce9029cb8cfc9b05
Tobin C. Harding <me@tobin.cc>
2023-08-08 11:40:24 +1000

    CI: Grep for exact error message
    `cargo +nightly` output of panic recently changed breaking our grep
    statement by adding the code line and a newline.
    
    Grep for the exact second line of the error message.

--- contrib/test.sh
+++ contrib/test.sh
@@ -20,7 +20,9 @@ if cargo --version | grep "1\.48"; then
 fi
 
 # Test if panic in C code aborts the process (either with a real panic or with SIGILL)
-cargo test -- --ignored --exact 'tests::test_panic_raw_ctx_should_terminate_abnormally' 2>&1 | tee /dev/stderr | grep "SIGILL\\|panicked at '\[libsecp256k1\]"
+cargo test -- --ignored --exact 'tests::test_panic_raw_ctx_should_terminate_abnormally' 2>&1 \
+    | tee /dev/stderr \
+    | grep "SIGILL\\|\[libsecp256k1] illegal argument. !rustsecp256k1_v0_._._fe_is_zero(&ge->x)"
 
 # Make all cargo invocations verbose
 export CARGO_TERM_VERBOSE=true
