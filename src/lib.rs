0da394e64848baf93c0aca0a9d6b5c9b2648b908
Tobin C. Harding <me@tobin.cc>
2024-03-19 18:22:13 +1100

    Remove wildcard re-exports of key types
    Wildcards make it hard to grep for where stuff comes from, explicit
    imports and re-exports are ... more explicit.
    
    Re-export the `key` types explicitly.

--- src/lib.rs
+++ src/lib.rs
@@ -192,7 +192,7 @@ pub use crate::context::{
 };
 use crate::ffi::types::AlignedType;
 use crate::ffi::CPtr;
-pub use crate::key::{PublicKey, SecretKey, *};
+pub use crate::key::{InvalidParityValue, Keypair, Parity, PublicKey, SecretKey, XOnlyPublicKey};
 pub use crate::scalar::Scalar;
 
 /// Trait describing something that promises to be a 32-byte random number; in particular,
