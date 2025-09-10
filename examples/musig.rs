70e928cb6a28598276b1c2d51b36fedfcab9d0ca
Tobin C. Harding <me@tobin.cc>
2025-09-10 13:45:59 +1000

    musig: Remove context
    We are removing the context everywhere we can, do so for the `musig`
    module.
    
    N.B. the seed initialization logic in `new_nonce_pair`.

--- examples/musig.rs
+++ examples/musig.rs
@@ -21,16 +21,16 @@ fn main() {
 
     secp.sort_pubkeys(pubkeys_ref);
 
-    let mut musig_key_agg_cache = KeyAggCache::new(&secp, pubkeys_ref);
+    let mut musig_key_agg_cache = KeyAggCache::new(pubkeys_ref);
 
     let plain_tweak: [u8; 32] = *b"this could be a BIP32 tweak....\0";
     let xonly_tweak: [u8; 32] = *b"this could be a Taproot tweak..\0";
 
     let plain_tweak = Scalar::from_be_bytes(plain_tweak).unwrap();
-    musig_key_agg_cache.pubkey_ec_tweak_add(&secp, &plain_tweak).unwrap();
+    musig_key_agg_cache.pubkey_ec_tweak_add(&plain_tweak).unwrap();
 
     let xonly_tweak = Scalar::from_be_bytes(xonly_tweak).unwrap();
-    let tweaked_agg_pk = musig_key_agg_cache.pubkey_xonly_tweak_add(&secp, &xonly_tweak).unwrap();
+    let tweaked_agg_pk = musig_key_agg_cache.pubkey_xonly_tweak_add(&xonly_tweak).unwrap();
 
     let agg_pk = musig_key_agg_cache.agg_pk();
 
@@ -41,7 +41,6 @@ fn main() {
     let musig_session_sec_rand1 = SessionSecretRand::from_rng(&mut rng);
 
     let nonce_pair1 = new_nonce_pair(
-        &secp,
         musig_session_sec_rand1,
         Some(&musig_key_agg_cache),
         Some(seckey1),
@@ -53,7 +52,6 @@ fn main() {
     let musig_session_sec_rand2 = SessionSecretRand::from_rng(&mut rng);
 
     let nonce_pair2 = new_nonce_pair(
-        &secp,
         musig_session_sec_rand2,
         Some(&musig_key_agg_cache),
         Some(seckey2),
@@ -72,22 +70,22 @@ fn main() {
     let nonces_ref: Vec<&PublicNonce> = nonces.iter().collect();
     let nonces_ref = nonces_ref.as_slice();
 
-    let agg_nonce = AggregatedNonce::new(&secp, nonces_ref);
+    let agg_nonce = AggregatedNonce::new(nonces_ref);
 
-    let session = Session::new(&secp, &musig_key_agg_cache, agg_nonce, msg);
+    let session = Session::new(&musig_key_agg_cache, agg_nonce, msg);
 
     let keypair1 = Keypair::from_secret_key(&seckey1);
-    let partial_sign1 = session.partial_sign(&secp, sec_nonce1, &keypair1, &musig_key_agg_cache);
+    let partial_sign1 = session.partial_sign(sec_nonce1, &keypair1, &musig_key_agg_cache);
 
     let keypair2 = Keypair::from_secret_key(&seckey2);
-    let partial_sign2 = session.partial_sign(&secp, sec_nonce2, &keypair2, &musig_key_agg_cache);
+    let partial_sign2 = session.partial_sign(sec_nonce2, &keypair2, &musig_key_agg_cache);
 
     let is_partial_signature_valid =
-        session.partial_verify(&secp, &musig_key_agg_cache, &partial_sign1, &pub_nonce1, pubkey1);
+        session.partial_verify(&musig_key_agg_cache, &partial_sign1, &pub_nonce1, pubkey1);
     assert!(is_partial_signature_valid);
 
     let is_partial_signature_valid =
-        session.partial_verify(&secp, &musig_key_agg_cache, &partial_sign2, &pub_nonce2, pubkey2);
+        session.partial_verify(&musig_key_agg_cache, &partial_sign2, &pub_nonce2, pubkey2);
     assert!(is_partial_signature_valid);
 
     let partial_sigs = [partial_sign1, partial_sign2];
