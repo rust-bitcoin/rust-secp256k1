bea8f94982d7bca850c9b63a09f9cf70af1bfef9
Tobin C. Harding <me@tobin.cc>
2025-09-11 13:57:54 +1000

    Re-export context from crate::context
    To be explicit and make the code brain dead easy to read.
    
    Internal change only.

--- src/lib.rs
+++ src/lib.rs
@@ -193,8 +193,7 @@ use core::{fmt, mem, str};
 pub use secp256k1_sys as ffi;
 
 #[cfg(all(feature = "global-context", feature = "std"))]
-pub use context::global::{self, SECP256K1};
-
+pub use crate::context::global::{self, SECP256K1};
 pub use crate::context::{
     rerandomize_global_context, with_global_context, with_raw_global_context, AllPreallocated,
     Context, PreallocatedContext, SignOnlyPreallocated, Signing, Verification,
